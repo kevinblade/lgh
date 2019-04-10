2ef07d2791667d0fc9ec7ab9d9df9266fcb14b4b
Kwangje Cho <kjcho@tapyn.com>
2019-04-10 16:39:47 +0900

    update kfctl.sh

--- kubeflow/kfctl.sh
+++ kubeflow/kfctl.sh
@@ -1,5 +1,6 @@
-mkdir ${KUBEFLOW_SRC}
+KUBEFLOW_TAG=v0.4.1
+KUBEFLOW_SRC="src/${KUBEFLOW_TAG}"
+mkdir -p ${KUBEFLOW_SRC}
 cd ${KUBEFLOW_SRC}
-export KUBEFLOW_TAG=v0.4.1
 
 curl https://raw.githubusercontent.com/kubeflow/kubeflow/${KUBEFLOW_TAG}/scripts/download.sh | bash
