94620ce1a56619659004b65ae073cabce10db633
Kwangje Cho <kjcho@tapyn.com>
2019-04-28 19:59:23 +0900

    Update for Jupyterhub

--- docker-images/no-gpu/jupyterhub/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t lgh-aml-container:no-gpu-jupyterhub .
--- docker-images/no-gpu/jupyterhub/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it -p 9200:8000 -v $HOME/data:/data lgh-aml-container:no-gpu-jupyterhub
