f5b70f891b74d8d45fffecdace11d03a675a6dfa
Kwangje Cho <kjcho@tapyn.com>
2019-04-28 20:35:15 +0900

    Update for Jupyterhub

---
+++ docker-images/no-gpu/aml/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+export JUPYTERHUB_RUNTIME_DIR=/srv/jupyterhub
+export JUPYTERHUB_CONFIG_FILE=jupyterhub_config.py
+
+jupyterhub -f ${JUPYTERHUB_RUNTIME_DIR}/${JUPYTERHUB_CONFIG_FILE}
