983fcee29983bbe980a9a58307feba0b39030a3f
Kwangje Cho <kjcho@tapyn.com>
2019-05-02 18:29:38 +0900

    Update files

--- docker-images/no-gpu/aml/run-aml.sh
+++ docker-images/no-gpu/aml/run-aml.sh
@@ -15,7 +15,7 @@ export GOGS_CONTAINER_NAME=aml-gogs
 export AML_CONTAINER_NAME=aml-container
 export AIRFLOW_UI_PORT=5601
 export AIRFLOW_LOCAL_MOUNT=/data/mlsapp/mls
-export AIRFLOW_CONTAINER_MOUNT=/home/airflow
+export AIRFLOW_CONTAINER_MOUNT=/home/mlsapp/mls
 
 # Jupyterhub 관련 환경 변수
 export JUPYTER_UI_PORT=8000
@@ -28,6 +28,9 @@ docker run -itd --name $AML_CONTAINER_NAME \
   --link $POSTGRES_CONTAINER_NAME:postgresql \
   -v $AIRFLOW_LOCAL_MOUNT:$AIRFLOW_CONTAINER_MOUNT \
   aml/anaconda3
+
+# MLS용 소스코드 폴더
+docker exec -it aml-container mkdir -p $AIRFLOW_CONTAINER_MOUNT
 
 # Airflow를 위한 PostgreSQL DB 초기화
 docker exec -it $AML_CONTAINER_NAME airflow initdb
