99b552cf9d765fdb0a192acf4c97684b653ac3e6
Kwangje Cho <kjcho@tapyn.com>
2019-05-02 16:49:00 +0900

    Add additional shell

--- docker-images/no-gpu/aml/run-aml.sh
+++ docker-images/no-gpu/aml/run-aml.sh
@@ -1,21 +1,15 @@
-# run-containers.sh
+# run-aml.sh
 #!/bin/bash
 
-# 기존 모든 컨테이너 종료 및 삭제
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+# 기존 컨테이너 종료 및 삭제
+docker stop aml-container
+docker rm aml-container
 
 # PostgreSQL 관련 환경 변수 설정
 export POSTGRES_CONTAINER_NAME=aml-postgres
-export POSTGRES_PORT=5432
-export POSTGRES_LOCAL_MOUNT=/data/pgdata
-export POSTGRES_CONTAINER_MOUNT=/var/lib/postgresql/data
 
 # Gogs 관련 환경 변수 설정
 export GOGS_CONTAINER_NAME=aml-gogs
-export GOGS_UI_PORT=10080
-export GOGS_LOCAL_MOUNT=/data/gogs
-export GOGS_CONTAINER_MOUNT=/data
 
 # AML 컨테이너 관련 환경 변수
 export AML_CONTAINER_NAME=aml-container
@@ -25,19 +19,6 @@ export AIRFLOW_CONTAINER_MOUNT=/home/airflow
 
 # Jupyterhub 관련 환경 변수
 export JUPYTER_UI_PORT=8000
-
-# PostgreSQL 컨테이너 실행
-docker run --rm -it -d --name $POSTGRES_CONTAINER_NAME \
-  -p $POSTGRES_PORT:5432 \
-  -v $POSTGRES_LOCAL_MOUNT:$POSTGRES_CONTAINER_MOUNT \
-  postgres
-
-# Gogs 컨테이너 실행
-docker run --rm -it -d --name $GOGS_CONTAINER_NAME \
-  -p 10022:22 \
-  -p $GOGS_UI_PORT:3000 \
-  -v $GOGS_LOCAL_MOUNT:$GOGS_CONTAINER_MOUNT \
-  gogs/gogs
 
 # AML 컨테이너 실행
 docker run -itd --name $AML_CONTAINER_NAME \
@@ -49,10 +30,12 @@ docker run -itd --name $AML_CONTAINER_NAME \
   aml/anaconda3
 
 # Airflow를 위한 PostgreSQL DB 초기화
-docker exec -it -d $AML_CONTAINER_NAME airflow initdb
+docker exec -it $AML_CONTAINER_NAME airflow initdb
 
 # Airflow 웹 서버 실행
 docker exec -it -d $AML_CONTAINER_NAME airflow webserver
 
 # Airflow scheduler 실행
 docker exec -it -d $AML_CONTAINER_NAME airflow scheduler
+
+docker logs $AML_CONTAINER_NAME
