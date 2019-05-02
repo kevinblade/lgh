e9301bfcb597ed163eaa6098b40efc8a11ccebdf
Kwangje Cho <kjcho@tapyn.com>
2019-05-02 15:28:08 +0900

    Update Dockerfile and add run-container.sh, airflow.cfg

--- docker-images/no-gpu/aml/run-conainers.sh
+++ docker-images/no-gpu/aml/run-conainers.sh
@@ -1,22 +1,29 @@
 # run-containers.sh
 #!/bin/bash
 
+# 기존 모든 컨테이너 종료 및 삭제
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
+# PostgreSQL 관련 환경 변수 설정
 export POSTGRES_CONTAINER_NAME=aml-postgres
 export POSTGRES_PORT=5432
 export POSTGRES_LOCAL_MOUNT=/data/pgdata
 export POSTGRES_CONTAINER_MOUNT=/var/lib/postgresql/data
 
+# Gogs 관련 환경 변수 설정
 export GOGS_CONTAINER_NAME=aml-gogs
 export GOGS_UI_PORT=10080
 export GOGS_LOCAL_MOUNT=/data/gogs
 export GOGS_CONTAINER_MOUNT=/data
 
+# AML 컨테이너 관련 환경 변수
 export AML_CONTAINER_NAME=aml-container
-
 export AIRFLOW_UI_PORT=5601
 export AIRFLOW_LOCAL_MOUNT=/data/mlsapp/mls
 export AIRFLOW_CONTAINER_MOUNT=/home/airflow
 
+# Jupyterhub 관련 환경 변수
 export JUPYTER_UI_PORT=8000
 
 # PostgreSQL 컨테이너 실행
@@ -41,9 +48,11 @@ docker run -itd --name $AML_CONTAINER_NAME \
   -v $AIRFLOW_LOCAL_MOUNT:$AIRFLOW_CONTAINER_MOUNT \
   aml/anaconda3
 
-# Initialize database for airflow
+# Airflow를 위한 PostgreSQL DB 초기화
 docker exec -it -d $AML_CONTAINER_NAME airflow initdb
-# Execute a web server for airflow
+
+# Airflow 웹 서버 실행
 docker exec -it -d $AML_CONTAINER_NAME airflow webserver
-# Execute a scheduler for airflow
+
+# Airflow scheduler 실행
 docker exec -it -d $AML_CONTAINER_NAME airflow scheduler
